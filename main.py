7939deb6e74d126f6535bfb83ecbaae7fd361bfa
itaigat <itaigat.mail@gmail.com>
2018-07-31 11:57:16 +0300

    Viterbi added

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from utils.decoder import CoNLLDataset, SportDataset
 from utils.common import zfill_files
+from models.algorithms import Viterbi
 
 
 if __name__ == "__main__":
+    """
     ds = CoNLLDataset('data\\CoNLL2003\\test.txt')
     print(ds)
     ds = SportDataset('data\\Sport5\\data')
     print(ds)
 
-    # zfill_files('data\\Sport5\\data')
+    zfill_files('data\\Sport5\\data')
+    """
+
+    vt = Viterbi.example()
---
+++ models/algorithms.py
@@ -0,0 +1,44 @@
+class Viterbi:
+    @staticmethod
+    def print(V):
+        for y in V[0].keys():
+            print('State:', y)
+            for t in range(len(V)):
+                print(V[t][y])
+
+    @staticmethod
+    def viterbi(obs, states, start_p, trans_p, emit_p):
+        V = [{}]
+        path = {}
+        for y in states:
+            V[0][y] = start_p[y] * emit_p[y][obs[0]]  # Initialize base cases (t == 0)
+            path[y] = [y]
+        for t in range(1, len(obs)):  # Run Viterbi for t > 0
+            V.append({})
+            newpath = {}
+            for y in states:
+                (prob, state) = max([(V[t - 1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in states])
+                V[t][y] = prob
+                newpath[y] = path[state] + [y]
+            path = newpath  # Don't need to remember the old paths
+
+        Viterbi.print(V)
+        prob, state = max([(V[len(obs) - 1][y], y) for y in states])
+
+        return prob, path[state]
+
+    @staticmethod
+    def example():
+        states = ('Rainy', 'Sunny')
+        observations = ('walk', 'shop', 'clean')
+        start_probability = {'Rainy': 0.6, 'Sunny': 0.4}
+        transition_probability = {
+            'Rainy': {'Rainy': 0.7, 'Sunny': 0.3},
+            'Sunny': {'Rainy': 0.4, 'Sunny': 0.6},
+        }
+        emission_probability = {
+            'Rainy': {'walk': 0.1, 'shop': 0.4, 'clean': 0.5},
+            'Sunny': {'walk': 0.6, 'shop': 0.3, 'clean': 0.1},
+        }
+
+        return Viterbi.viterbi(observations, states, start_probability, transition_probability, emission_probability)
