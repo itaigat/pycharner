0859ea01ba21dea9c1f5497a584d836d86a0783f
itaigat <itaigat.mail@gmail.com>
2018-08-13 13:58:51 +0300

    Paths on parameters

--- models/paramaters.py
+++ models/paramaters.py
@@ -1,4 +1,6 @@
+from os import path
+
 class paths:
-    CoNLLDataset_train_path = 'data\\CoNLL2003\\train.txt'
-    CoNLLDataset_test_path = 'data\\CoNLL2003\\test.txt'
-    CoNLLDataset_valid_path = 'data\\CoNLL2003\\valid.txt'
+    CoNLLDataset_train_path = path.join('data', 'CoNLL2003', 'train.txt')
+    CoNLLDataset_test_path = path.join('data', 'CoNLL2003', 'test.txt')
+    CoNLLDataset_valid_path = path.join('data', 'CoNLL2003', 'valid.txt')
